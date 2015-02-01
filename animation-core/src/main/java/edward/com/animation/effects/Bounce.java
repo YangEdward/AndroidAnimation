c41857a89cf41e9eef7de431d50b35f254d87830
YangEdward <murongyunlong@gmail.com>
2015-02-01 13:30:00 +0800

    just create

--- animation-core/src/main/java/edward/com/animation/effects/Bounce.java
+++ animation-core/src/main/java/edward/com/animation/effects/Bounce.java
@@ -4,17 +4,20 @@ import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.view.View;
 
+import edward.com.animation.impl.Effect4View;
+
 /**
  * Created by Edward on 2015/2/1.
  */
-public class Bounce extends SingleViewAnimator{
+public class Bounce implements Effect4View{
+
     @Override
-    protected Animator[] getAnimators(View target) {
+    public Animator[] getAnimators(View target) {
         return new Animator[]{ObjectAnimator.ofFloat(target, "translationY", 0, 0, -30, 0, -15, 0, 0)};
     }
 
     @Override
-    protected void reset(View target) {
+    public void reset(View target) {
         target.setTranslationY(0);
     }
 }
---
+++ animation-core/src/main/java/edward/com/animation/impl/Effect4View.java
@@ -0,0 +1,12 @@
+package edward.com.animation.impl;
+
+import android.animation.Animator;
+import android.view.View;
+
+/**
+ * Created by Edward on 2015/2/1.
+ */
+public interface Effect4View {
+    Animator[] getAnimators(View target);
+    void reset(View target);
+}
