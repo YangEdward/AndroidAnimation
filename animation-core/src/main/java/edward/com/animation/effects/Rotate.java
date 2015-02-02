b5ea075c7357c5adb0258061059b0de12b221865
YangEdward <murongyunlong@gmail.com>
2015-02-02 16:18:29 +0800

    creating

--- animation-core/src/main/java/edward/com/animation/effects/Rotate.java
+++ animation-core/src/main/java/edward/com/animation/effects/Rotate.java
@@ -2,7 +2,6 @@ package edward.com.animation.effects;
 
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
-import android.support.v4.view.ViewPager;
 import android.view.View;
 
 import edward.com.animation.enums.Action;
@@ -10,12 +9,10 @@ import edward.com.animation.enums.Direction;
 import edward.com.animation.impl.Effect4View;
 import edward.com.animation.impl.HasAction;
 import edward.com.animation.impl.HasDirection;
-import edward.com.animation.utils.LayerUtil;
 
 public class Rotate implements Effect4View,HasAction,HasDirection{
 
     private static final float ROT_MAX = 15.0f;
-    private ViewPager pager;
     private Direction direction;
     private Action action = Action.IN;
 
@@ -26,31 +23,6 @@ public class Rotate implements Effect4View,HasAction,HasDirection{
     public Rotate(Direction direction,Action action){
         this.direction = direction;
         this.action = action;
-    }
-
-	protected void setAnimations(View left, View right, float positionOffset,boolean up) {
-		if (left != null) {
-			LayerUtil.manageLayer(left, true);
-			float mRot = (up ? 1 : -1) * (ROT_MAX * positionOffset);
-			float mTrans = (up ? -1 : 1) * (float) (pager.getMeasuredHeight() - pager.getMeasuredHeight()*Math.cos(mRot*Math.PI/180.0f));
-            left.setPivotX(left.getMeasuredWidth()*0.5f);
-            left.setPivotY(up ? 0 : left.getMeasuredHeight());
-            left.setTranslationY(mTrans);
-            left.setRotation(mRot);
-		}
-		if (right != null) {
-			LayerUtil.manageLayer(right, true);
-			float mRot = (up ? 1 : -1) * (-ROT_MAX + ROT_MAX*positionOffset);
-			float mTrans = (up ? -1 : 1) * (float) (pager.getMeasuredHeight() - pager.getMeasuredHeight()*Math.cos(mRot*Math.PI/180.0f));
-            right.setPivotX(right.getMeasuredWidth()*0.5f);
-            right.setPivotY(up ? 0 : right.getMeasuredHeight());
-            right.setTranslationY(mTrans);
-            right.setRotation(mRot);
-		}
-	}
-
-    protected void setViewPager(ViewPager pager){
-        this.pager = pager;
     }
 
     @Override
