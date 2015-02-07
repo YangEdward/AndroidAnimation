f7d0b3ec3358574d48825d4411e10938c3fada36
YangEdward <murongyunlong@gmail.com>
2015-02-07 23:15:15 +0800

    add animation for recycler view

---
+++ animation-recyclerview/src/main/java/edward/com/recyclerview/ScrollAnimatorHelper.java
@@ -0,0 +1,21 @@
+
+package edward.com.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+
+public class ScrollAnimatorHelper extends RecyclerView.OnScrollListener {
+
+    public ScrollAnimatorHelper() {
+        super();
+    }
+
+    @Override
+    public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+        super.onScrollStateChanged(recyclerView, newState);
+    }
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+    }
+}
